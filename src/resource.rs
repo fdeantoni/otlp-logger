c0f14582adf37fc11ae9d5afcd4606c1a59a2610
satyam <satyamjay030@gmail.com>
2024-11-24 15:36:01 +0530

    update crates version

--- src/resource.rs
+++ src/resource.rs
@@ -4,13 +4,15 @@ use std::env::args_os;
 use std::process::id;
 
 use opentelemetry::{KeyValue, StringValue, Value};
-use opentelemetry_sdk::{resource::{ResourceDetector, SdkProvidedResourceDetector, TelemetryResourceDetector}, Resource};
+use opentelemetry_sdk::{
+    resource::{ResourceDetector, SdkProvidedResourceDetector, TelemetryResourceDetector},
+    Resource,
+};
 use opentelemetry_semantic_conventions::resource as otel_resource;
 
 use crate::OtlpConfig;
 
 pub fn otel_resource(config: &OtlpConfig) -> Resource {
-
     let os_resource = detect_os();
     let process_resource = detect_process();
     let telemetry_resource = TelemetryResourceDetector.detect(Duration::from_secs(0));
@@ -18,19 +20,34 @@ pub fn otel_resource(config: &OtlpConfig) -> Resource {
 
     let mut provided = Vec::new();
     if let Some(service_name) = &config.service_name {
-        provided.push(KeyValue::new(otel_resource::SERVICE_NAME, service_name.clone()));
+        provided.push(KeyValue::new(
+            otel_resource::SERVICE_NAME,
+            service_name.clone(),
+        ));
     }
     if let Some(service_namespace) = &config.service_namespace {
-        provided.push(KeyValue::new(otel_resource::SERVICE_NAMESPACE, service_namespace.clone()));
+        provided.push(KeyValue::new(
+            otel_resource::SERVICE_NAMESPACE,
+            service_namespace.clone(),
+        ));
     }
     if let Some(service_version) = &config.service_version {
-        provided.push(KeyValue::new(otel_resource::SERVICE_VERSION, service_version.clone()));
+        provided.push(KeyValue::new(
+            otel_resource::SERVICE_VERSION,
+            service_version.clone(),
+        ));
     }
     if let Some(service_instant_id) = &config.service_instant_id {
-        provided.push(KeyValue::new(otel_resource::SERVICE_INSTANCE_ID, service_instant_id.clone()));
+        provided.push(KeyValue::new(
+            otel_resource::SERVICE_INSTANCE_ID,
+            service_instant_id.clone(),
+        ));
     }
     if let Some(deployment_environment) = &config.deployment_environment {
-        provided.push(KeyValue::new(otel_resource::DEPLOYMENT_ENVIRONMENT_NAME, deployment_environment.clone()));
+        provided.push(KeyValue::new(
+            otel_resource::DEPLOYMENT_ENVIRONMENT_NAME,
+            deployment_environment.clone(),
+        ));
     }
 
     let app = Resource::new(provided);
@@ -43,7 +60,10 @@ pub fn otel_resource(config: &OtlpConfig) -> Resource {
 }
 
 fn detect_os() -> Resource {
-    Resource::new(vec![KeyValue::new(otel_resource::OS_TYPE, std::env::consts::OS)])
+    Resource::new(vec![KeyValue::new(
+        otel_resource::OS_TYPE,
+        std::env::consts::OS,
+    )])
 }
 
 fn detect_process() -> Resource {
@@ -52,7 +72,9 @@ fn detect_process() -> Resource {
         .into_iter()
         .map(|arg| arg.to_string_lossy().into_owned().into())
         .collect::<Vec<StringValue>>();
-    let current_exe = std::env::current_exe().map(|exe| exe.display().to_string()).unwrap_or_default();
+    let current_exe = std::env::current_exe()
+        .map(|exe| exe.display().to_string())
+        .unwrap_or_default();
     Resource::new(vec![
         KeyValue::new(
             opentelemetry_semantic_conventions::resource::PROCESS_COMMAND_ARGS,
@@ -62,9 +84,6 @@ fn detect_process() -> Resource {
             opentelemetry_semantic_conventions::resource::PROCESS_PID,
             id() as i64,
         ),
-        KeyValue::new(
-            otel_resource::PROCESS_EXECUTABLE_NAME, 
-            current_exe
-        ),
+        KeyValue::new(otel_resource::PROCESS_EXECUTABLE_NAME, current_exe),
     ])
 }
